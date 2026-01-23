7063fdece29b0694ba974aa9c583f523c530927f
turgitx78 <turgutazizli3@gmail.com>
2026-01-23 14:32:34 -0500

    Fix: ensure exactly 2 lines

--- linux_security/0x00_linux_security_basics/2-incoming_connections.sh
+++ linux_security/0x00_linux_security_basics/2-incoming_connections.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-# IPv4 üçün port 80-ə TCP daxil olan bağlantılara icazə ver
-sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
-echo "Rules updated"
-
-# IPv6 üçün port 80-ə TCP daxil olan bağlantılara icazə ver
-sudo ip6tables -A INPUT -p tcp --dport 80 -j ACCEPT
-echo "Rules updated (v6)"
+sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT && echo "Rules updated" && sudo ip6tables -A INPUT -p tcp --dport 80 -j ACCEPT && echo "Rules updated (v6)"
