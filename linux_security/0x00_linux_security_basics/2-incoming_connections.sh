22e77ea21859d6997180ac579bccd7dc19a329aa
turgitx78 <turgutazizli3@gmail.com>
2026-01-23 14:33:17 -0500

    Use ufw to allow port 80 TCP

--- linux_security/0x00_linux_security_basics/2-incoming_connections.sh
+++ linux_security/0x00_linux_security_basics/2-incoming_connections.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT && echo "Rules updated" && sudo ip6tables -A INPUT -p tcp --dport 80 -j ACCEPT && echo "Rules updated (v6)"
+sudo ufw allow 80/tcp
