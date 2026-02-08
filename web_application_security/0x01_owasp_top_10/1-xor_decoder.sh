f0abb32529a57f61810eeb57eeb495c0be750bb7
turgitx78 <turgutazizli3@gmail.com>
2026-02-08 12:42:16 -0500

    Add XOR WebSphere decoder script

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-
-# Hash-dən {xor} hissəsini silirik
-input_hash=$(echo "$1" | sed 's/^{xor}//')
-
-# Kodlaşdırırıq: Base64 decode -> XOR 95 (_) -> Yeni sətir olmadan çap et
-echo -n "$input_hash" | base64 -d | perl -pe 's/./$_ ^ "_" /ge'
+python3 -c "from base64 import b64decode; print(bytes(byte ^ 0x5f for byte in b64decode('$1'.replace('{xor}', ''))).decode('utf-8'))"
