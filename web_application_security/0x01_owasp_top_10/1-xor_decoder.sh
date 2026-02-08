1f8d0e76b7fa9c7c7cc63059a250936647a820f6
turgitx78 <turgutazizli3@gmail.com>
2026-02-08 12:20:51 -0500

    Add XOR WebSphere decoder script

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# Hash-in başındakı {xor} hissəsini təmizləyirik
+# Hash-dən {xor} hissəsini silirik
 input_hash=$(echo "$1" | sed 's/^{xor}//')
 
-# Base64-dən çıxarırıq və hər simvolu ASCII 95 (_) ilə XOR edirik
-echo "$input_hash" | base64 -d | perl -pe 's/./$_ ^ "_" /ge'
-echo ""
+# Kodlaşdırırıq: Base64 decode -> XOR 95 (_) -> Yeni sətir olmadan çap et
+echo -n "$input_hash" | base64 -d | perl -pe 's/./$_ ^ "_" /ge'
